c8fc46d9d7e84a9feae29440765e13b0364b30b8
syna-ag-atoconn <syna.agarwala@atoconn.com>
2026-05-02 17:42:39 +0530

    fixes should be final

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
 import { app } from '../src/app';
-import { env } from '../src/config/env';
+// import { env } from '../src/config/env';
 
-if (!process.env.VERCEL) {
-  app.listen(env.port, () => {
-    console.log(`API server listening on port ${env.port}`);
-  });
-}
+// if (!process.env.VERCEL) {
+//   app.listen(env.port, () => {
+//     console.log(`API server listening on port ${env.port}`);
+//   });
+// }
 
 export default app;
